e8f1964b3d0af366e690a21ddde4ac462b079c85
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2023-05-19 13:12:42 +0300

    Code cleanup

--- main.js
+++ main.js
@@ -1,72 +1,37 @@
 import {
+  sudokuMatrix,
   generateArray,
   diagonalSudokuFill,
   fillSudokuBoard,
   checkIfCorrect,
 } from "./utils/board-generation.js";
+import { createChoices, unsolveTheBoard } from "./utils/qol-functions.js";
 
-// generateTable();
-
-sudokuMatrix();
-function sudokuMatrix(dimensions = [9, 9], empty = false) {
-  // [9, 9] matrix
-  let baseMatrix;
-
-  if (empty == true) {
-    baseMatrix = Array(dimensions[0])
-      .fill()
-      .map(() => Array(dimensions[1]).fill(0));
-    return baseMatrix;
-  }
-
-  let i = 0;
-  baseMatrix = Array(dimensions[0])
-    .fill()
-    .map(() => Array(dimensions[1]).fill(0));
-  diagonalSudokuFill(baseMatrix); // Fill 3 diagonal sudoku 9x9 blocks, left top to right bottom
-  fillSudokuBoard(baseMatrix, 0, 0); // Fill the rest of the blocks recursively, using Backtracking algorithm
-  console.log(baseMatrix);
-}
+generateTable();
 
 function generateTable() {
-  // [[0-0],[0-1],...,[9-8],[9-9]] - row id, cell id
-  var baseMatrix = sudokuMatrix([9, 9], true);
+  let baseMatrix = sudokuMatrix();
   var table = document.createElement("table");
-  var tableBody = document.createElement("tbody");
   var nums = generateArray(1, 9, false);
 
-  var i = 0;
-  baseMatrix.forEach(function (rowData) {
-    var row = document.createElement("tr");
+  unsolveTheBoard(baseMatrix, 17);
 
-    var j = 0;
-    rowData.forEach(function (cellData) {
-      var cell = document.createElement("td");
-      var select = document.createElement("select");
+  for (let row = 0; row < baseMatrix.length; row++) {
+    let tr = document.createElement("tr");
+
+    for (let col = 0; col < baseMatrix[row].length; col++) {
+      let td = document.createElement("td");
+      let select = document.createElement("select");
       select.addEventListener("change", checkIfCorrect);
-      select.id = i.toString() + "-" + j.toString();
+      select.id = row.toString() + "-" + col.toString();
 
-      nums.forEach(function (choice) {
-        var option = document.createElement("option");
-        var txt = document.createTextNode(choice);
+      createChoices(select, nums, baseMatrix[row][col]);
 
-        option.appendChild(txt);
-        option.setAttribute("value", choice);
-        select.appendChild(option);
-      });
-      j++;
-      cell.appendChild(select);
-      row.appendChild(cell);
-    });
+      td.appendChild(select);
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
 
-    tableBody.appendChild(row);
-    i++;
-  });
-
-  table.appendChild(tableBody);
-  let tableContainer = document
-    .getElementById("table-container")
-    .appendChild(table);
-
-  return table;
+  document.getElementById("table-container").appendChild(table);
 }
