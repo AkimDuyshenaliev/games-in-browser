8d192f8b0854d72d920f27cb30855f9ae669e19a
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2023-05-24 17:32:21 +0300

    Done board generation and onClick event listeners, left->changing chosen number

--- main.js
+++ main.js
@@ -3,35 +3,54 @@ import {
   generateArray,
   diagonalSudokuFill,
   fillSudokuBoard,
-  checkIfCorrect,
 } from "./utils/board-generation.js";
-import { createChoices, unsolveTheBoard } from "./utils/qol-functions.js";
+import {
+  createChoices,
+  removeChoices,
+  unsolveTheBoard,
+  checkIfCorrect,
+  cellObserver,
+} from "./utils/qol-functions.js";
 
-generateTable();
+sudokuBoard();
 
-function generateTable() {
-  let baseMatrix = sudokuMatrix();
-  var table = document.createElement("table");
-  var nums = generateArray(1, 9, false);
+function sudokuBoard() {
+  let choices = generateArray(0, 10, false);
+  let matrix = sudokuMatrix();
+  unsolveTheBoard(matrix, 17);
 
-  unsolveTheBoard(baseMatrix, 17);
+  for (let row = 0; row < matrix.length; row++) {
+    let rowBox = document.createElement("div");
+    rowBox.classList.add("flex-container");
 
-  for (let row = 0; row < baseMatrix.length; row++) {
-    let tr = document.createElement("tr");
+    for (let col = 0; col < matrix[row].length; col++) {
+      let cell = document.createElement("div");
+      cell.classList.add("flex-container-cells");
 
-    for (let col = 0; col < baseMatrix[row].length; col++) {
-      let td = document.createElement("td");
-      let select = document.createElement("select");
-      select.addEventListener("change", checkIfCorrect);
-      select.id = row.toString() + "-" + col.toString();
+      if (matrix[row][col] == 0) {
+        cell.style.setProperty("background-color", "white", "");
+        cell.innerHTML = "";
+        cell.addEventListener("click", (event) => {
+          createChoices(event, choices, cell);
+        });
 
-      createChoices(select, nums, baseMatrix[row][col]);
+        cellObserver(cell); //Observe changes in innerHTML and remove choices when number selected
+      } else {
+        cell.style.setProperty("background-color", "lightgrey", "");
+        cell.innerHTML = matrix[row][col];
+      }
 
-      td.appendChild(select);
-      tr.appendChild(td);
+      let borderRow = row % 3 == false && row != 0 && row != 8;
+      let borderCol = col % 3 == false && col != 0 && col != 8;
+      if (borderRow) {
+        cell.classList.add("top-border");
+      }
+      if (borderCol) {
+        cell.classList.add("left-border");
+      }
+      cell.id = row + "-" + col;
+      rowBox.appendChild(cell);
     }
-    table.appendChild(tr);
+    document.getElementById("custom-select").appendChild(rowBox);
   }
-
-  document.getElementById("table-container").appendChild(table);
 }
