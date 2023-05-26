994dcee1b2d0892d4815626062a94800f94e623c
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2023-05-26 16:38:40 +0300

    Building menu and sudoku board from classes

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 import { sudokuBoard } from "./utils/sudoku-board.js";
 import { SudokuBoard } from "./classes/class-sudoku-board.js";
+import { MainMenu } from "./classes/class-main-menu.js";
 
 // Sudoku board from a function
 // sudokuBoard(); 
 
 // Sudoku board from classes
-const board = new SudokuBoard(document, "Pure node.js Sudoku");
-board.buildBoard("main");
+// const board = new SudokuBoard(document);
+// board.buildBoard("main");
+
+// Main menu before the game
+const menu = new MainMenu(document);
+menu.buildMenu("div", "main")
